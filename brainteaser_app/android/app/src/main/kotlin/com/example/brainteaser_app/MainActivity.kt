a9b25c37beb1724c6be6e96788977e43e6d73a4c
Wesley Kibagendi <wesoon98@gmail.com>
2022-10-31 12:49:35 +0300

    Basic app architecture

---
+++ brainteaser_app/android/app/src/main/kotlin/com/example/brainteaser_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.brainteaser_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ brainteaser_app/lib/screens/answers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Answer extends StatelessWidget {
+  const Answer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
